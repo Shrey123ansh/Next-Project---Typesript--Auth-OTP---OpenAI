4e128e2314e04cc26440f2156f671ce618b81a6a
Shrey123ansh <shreyann56sh@gmail.com>
2024-06-27 17:52:11 +0530

    check-username-unique

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -14,7 +14,7 @@ export default function Component() {
     <>
       Not signed in <br />
       <button
-        className="bg-orange-500 px-3 py-1 m-4 rounded "
+        className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
         onClick={() => signIn()}
       >
         Sign in
